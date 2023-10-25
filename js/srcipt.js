57c5509e85b475a669075d64d386d8b7fe2684f8
Diego Rubio <diegorubio1704.drs@gmail.com>
2023-10-25 12:16:59 +0200

    update design

--- js/srcipt.js
+++ js/srcipt.js
@@ -1,3 +1,16 @@
+const avatar = [
+    "capitanamerica",
+    "spiderman",
+    "loki",
+    "thor",
+    "ironman",
+    "wolverine",
+    "deadpool",
+    "gru",
+    "hulk",
+    "nickfuria",
+    "scarlet"
+]
 const config__avatar = document.getElementById("config__avatar")
 const img__perfil = document.getElementById("img__perfil")
 
@@ -7,14 +20,18 @@ const span__name = document.getElementById("span__name")
 const name__input = document.getElementById("name__input");
 const name__button = document.getElementById("name__button");
 
+
+
 //escoger nombre
 const selectName = (event) =>{
 
-    if(name__input.value == ""){
+    if(name__input.value === ""){
         text__error.classList.add("displayblock")
+        nombre.classList.remove("displayblock")
+        nombre.classList.add("displaynone")
     }else{
         nombre.classList.add("displayblock")
-        text__error.classList.remove("displaynone")
+        text__error.classList.remove("displayblock")
         span__name.textContent = name__input.value
     }
 }
@@ -33,3 +50,19 @@ const selectAvatar = (event) =>{
 }
 
 config__avatar.addEventListener("click", selectAvatar);
+
+
+const loadImg = (event) =>{
+
+    for(let i = 0; i<6; i++){
+        let indiceAle = Math.floor(Math.random()*avatar.length)
+        let imgAle = avatar.splice(indiceAle,1)[0];
+        let img = document.createElement("IMG");
+        img.className = "config__img";
+        img.src = "./assets/images/avatares/"+imgAle+".png";
+        config__avatar.appendChild(img)
+    }
+
+}
+
+document.addEventListener("DOMContentLoaded", loadImg)
