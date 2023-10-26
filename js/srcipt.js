2ccfa59c148c65397097701003b7089be188410c
Diego Rubio <diegorubio1704.drs@gmail.com>
2023-10-26 10:09:29 +0200

    validate config

--- js/srcipt.js
+++ js/srcipt.js
@@ -32,32 +32,36 @@ const button__preguntas = document.getElementById("button__preguntas");
 let existeName
 let existeAvatar
 
+let nombreSelecc
 //escoger nombre
 const selectName = (event) =>{
-
+    
     if(name__input.value === ""){
-        text__error.classList.add("displayblock")
         nombre.classList.remove("displayblock")
         nombre.classList.add("displaynone")
     }else{
+        nombreSelecc = event.target
         nombre.classList.add("displayblock")
-        text__error.classList.remove("displayblock")
         span__name.textContent = name__input.value
         existeName = span__name.textContent;
+        nombre__jugador.classList.remove("error")
     }
 }
 
 name__button.addEventListener("click", selectName)
 
+let avatarSelecc
 //escoger avatar
 const selectAvatar = (event) =>{
 
     let element = event.target;
     
     if(element.nodeName === "IMG"){
+        avatarSelecc = event.target;
         img__perfil.classList.remove("displaynone")
         img__perfil.src = element.src
         existeAvatar = img__perfil.src 
+        avatar__jugador.classList.remove("error")
     }
 }
 
@@ -79,18 +83,44 @@ const loadImg = (event) =>{
 
 document.addEventListener("DOMContentLoaded", loadImg)
 
+//validar el juego
+const validateConf = (event) =>{
+    if(!nombreSelecc){
+        nombre__jugador.classList.add("error")
+    }
+    if(!avatarSelecc){
+        avatar__jugador.classList.add("error")
+    }
+
+}
+
+button__pregunta.addEventListener("click", validateConf)
+
 //esconder la configuracion y pasar a la siguiente pantalla
 const loadGame = (event) =>{
 
     if(existeAvatar && existeName){
         config.classList.add("displaynone");
         eleccion__preguntas.classList.add("displayblock")
-        
         button__preguntas.classList.add("displaynone")
         button__juego.classList.remove("displaynone")
     }
-    
-
 }
 
 button__pregunta.addEventListener("click", loadGame)
+
+//seleccionar el oponente contra quien juegas
+const selectOponent = (event) =>{
+    console.log(event)
+    let element = event.target;
+    if(element.nodeName === "IMG"){
+      img__thanos.classList.add("background-purple")
+      random.classList.remove("background-purple")
+    }else if(element.nodeName === "I"){
+        img__thanos.classList.remove("background-purple")
+        random.classList.add("background-purple")
+    }
+  
+}
+  
+article__eleccion.addEventListener("click", selectOponent)
